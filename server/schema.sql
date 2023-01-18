cefadd100ce6549106da00a58a37d239d721e5e9
Midori <midorili998@gmail.com>
2023-01-17 20:56:33 -0800

    Partial Completion Tuesday Night

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,40 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID int NOT NULL,
+  Message varchar(500),
+  PRIMARY KEY (ID)
 );
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  ID int NOT NULL,
+  User varchar(255),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  ID int NOT NULL,
+  Room varchar(255),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE messages_users_rooms (
+  /* Describe your table here.*/
+  ID int NOT NULL,
+  Message_ID int,
+  User_ID int,
+  Room_ID int,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (Message_ID) REFERENCES messages(ID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (User_ID) REFERENCES users(ID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (Room_ID) REFERENCES rooms(ID)
+    ON DELETE CASCADE
+);
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
