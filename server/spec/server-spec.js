ab35eecf8a0348672fb8ac54dc1117cbe233651f
Midori <midorili998@gmail.com>
2023-01-21 11:44:38 -0800

    "Complete adding additional unit tests"

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -1,21 +1,21 @@
 /* You'll need to have MySQL running and your Node server running
  * for these tests to pass. */
 
-const mysql = require("mysql2");
-const axios = require("axios");
+const mysql = require('mysql2');
+const axios = require('axios');
 
-const API_URL = "http://127.0.0.1:3000/classes";
+const API_URL = 'http://127.0.0.1:3000/classes';
 
-describe("Persistent Node Chat Server", () => {
+describe('Persistent Node Chat Server', () => {
   const dbConnection = mysql.createConnection({
-    user: "root",
-    database: "chat",
+    user: 'root',
+    database: 'chat',
   });
 
   beforeAll((done) => {
     dbConnection.connect();
 
-    const tablename = "messages"; // TODO: fill this out
+    const tablename = 'messages'; // TODO: fill this out
 
     /* Empty the db table before all tests so that multiple tests
      * (or repeated runs of the tests)  will not fail when they should be passing
@@ -27,10 +27,10 @@ describe("Persistent Node Chat Server", () => {
     dbConnection.end();
   });
 
-  it("Should insert posted messages to the DB", (done) => {
-    const username = "Valjean";
-    const message = "In mercy's name, three days is all I need.";
-    const roomname = "Hello";
+  it('Should insert posted messages to the DB', (done) => {
+    const username = 'Valjean';
+    const message = 'In mercy\'s name, three days is all I need.';
+    const roomname = 'Hello';
     // Create a user on the chat server database.
     axios
       .post(`${API_URL}/users`, { 'username': username })
@@ -47,7 +47,7 @@ describe("Persistent Node Chat Server", () => {
 
         /* TODO: You might have to change this test to get all the data from
          * your message table, since this is schema-dependent. */
-        const queryString = "SELECT * FROM messages";
+        const queryString = 'SELECT * FROM messages';
         const queryArgs = [];
 
         dbConnection.query(queryString, queryArgs, (err, results) => {
@@ -67,14 +67,14 @@ describe("Persistent Node Chat Server", () => {
       });
   });
 
-  it("Should output all messages from the DB", (done) => {
+  it('Should output all messages from the DB', (done) => {
     // Let's insert a message into the db
 
     //****NOTE TO SELVES: PROBABLY NEED TO ADD ROOM AND USER ***
 
     const queryString =
-      "INSERT INTO messages (username, message, roomname) VALUES (?, ?, ?)";
-    const queryArgs = ["Valjean", "In mercy's name, three days is all I need.", "Hello"];
+      'INSERT INTO messages (username, message, roomname) VALUES (?, ?, ?)';
+    const queryArgs = ['Valjean', 'In mercy\'s name, three days is all I need.', 'Hello'];
     /* TODO: The exact query string and query args to use here
      * depend on the schema you design, so I'll leave them up to you. */
     dbConnection.query(queryString, queryArgs, (err) => {
@@ -89,6 +89,31 @@ describe("Persistent Node Chat Server", () => {
           const messageLog = response.data;
           expect(messageLog[0].message).toEqual(queryArgs[1]);
           expect(messageLog[0].roomname).toEqual(queryArgs[2]);
+          done();
+        })
+        .catch((err) => {
+          throw err;
+        });
+    });
+  });
+
+  it('Should output all users from the DB', (done) => {
+    const queryString =
+    'INSERT INTO users (username) VALUES (?)';
+    const queryArgs = ['Midori'];
+    /* TODO: The exact query string and query args to use here
+   * depend on the schema you design, so I'll leave them up to you. */
+    dbConnection.query(queryString, queryArgs, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      // Now query the Node chat server and see if it returns the message we just inserted:
+      axios
+        .get(`${API_URL}/users`)
+        .then((response) => {
+          const username = response.data;
+          expect(username[0].username).toEqual(queryArgs[0]);
           done();
         })
         .catch((err) => {
